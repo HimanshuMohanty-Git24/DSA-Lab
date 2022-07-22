747dfeabccd3ba2c98f5ad93cde49a028a761ab6
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-22 23:46:45 +0530

    DSA Ques Day1 ques

--- ASSIGNMEN0/Q1.c
+++ ASSIGNMEN0/Q1.c
@@ -2,34 +2,40 @@
 // 2105719
 // CSE36
 #include <stdio.h>
-void splitString(char s[100],int query,int flag,int n){
-    for(int i=0;i<n;i++)
+void splitString(char s[100], int query, int flag, int n)
+{
+    for (int i = 0; i < n; i++)
     {
-        if(s[i]=='.'){
-            flag=i;
+        if (s[i] == '.')
+        {
+            flag = i;
             break;
         }
     }
-    if(query==0){
-        for(int j=0;j<flag;j++){
-            printf("%c",s[j]);
+    if (query == 0)
+    {
+        for (int j = 0; j < flag; j++)
+        {
+            printf("%c", s[j]);
         }
     }
-    else if(query==1){
-        for(int z=flag+1;s[z] != '\0';z++){
-            printf("%c",s[z]);
+    else if (query == 1)
+    {
+        for (int z = flag + 1; s[z] != '\0'; z++)
+        {
+            printf("%c", s[z]);
         }
     }
 }
 int main()
 {
     char s[100];
-    int query,flag;
+    int query, flag;
     printf("Enter a string:");
-    scanf("%s",s);
+    scanf("%s", s);
     printf("Enter the integer to print the specific part:");
-    scanf("%d",&query);
+    scanf("%d", &query);
     int n = sizeof(s) / sizeof(s[0]);
-    splitString(s,query,flag,n);
+    splitString(s, query, flag, n);
     return 0;
 }
