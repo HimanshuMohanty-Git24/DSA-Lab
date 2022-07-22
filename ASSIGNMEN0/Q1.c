1b630f7da837e37065f63f257c473bab7f154295
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-22 22:15:42 +0530

    DSA Ques Day1

--- ASSIGNMEN0/Q1.c
+++ ASSIGNMEN0/Q1.c
@@ -1,31 +1,36 @@
-//Himanshu Mohanty
-//2105719
-//CSE36
+// Himanshu Mohanty
+// 2105719
+// CSE36
 #include <stdio.h>
 
 int main()
 {
     char s[100];
-    int query,flag;
+    int query, flag;
     printf("Enter a string:");
-    scanf("%s",s);
+    scanf("%s", s);
     printf("Enter the integer to print the specific part:");
-    scanf("%d",&query);
-    for(int i=0;i<sizeof(s);i++)
+    scanf("%d", &query);
+    for (int i = 0; i < sizeof(s); i++)
     {
-        if(s[i]=='.'){
-            flag=i;
+        if (s[i] == '.')
+        {
+            flag = i;
             break;
         }
     }
-    if(query==0){
-        for(int j=0;j<flag;j++){
-            printf("%c",s[j]);
+    if (query == 0)
+    {
+        for (int j = 0; j < flag; j++)
+        {
+            printf("%c", s[j]);
         }
     }
-    else if(query==1){
-        for(int z=flag+1;s[z] != '\0';z++){
-            printf("%c",s[z]);
+    else if (query == 1)
+    {
+        for (int z = flag + 1; s[z] != '\0'; z++)
+        {
+            printf("%c", s[z]);
         }
     }
     return 0;
--- ASSIGNMEN0/Q2.c
+++ ASSIGNMEN0/Q2.c
@@ -1,48 +1,54 @@
-//Himanshu Mohanty
-//2105719
-//CSE36
+// Himanshu Mohanty
+// 2105719
+// CSE36
 #include <stdio.h>
 
 int main()
 {
-    char s[100],op,temp;
+    char s[100], op, temp;
     int flag[100];
-    int onesCount = 0; 
+    int onesCount = 0;
     int query;
     printf("Enter a string:");
-    scanf("%s",s);
-    scanf("%c",&temp);
+    scanf("%s", s);
+    scanf("%c", &temp);
     printf("Enter the operator to split:");
-    scanf("%c",&op);
+    scanf("%c", &op);
     printf("Enter the integer to print the specific part:");
-    scanf("%d",&query);
-    for(int i=0;i<sizeof(s);i++)
-    { 
-        if(s[i]==op){
-           flag[onesCount] = i;
-           onesCount++;
+    scanf("%d", &query);
+    for (int i = 0; i < sizeof(s); i++)
+    {
+        if (s[i] == op)
+        {
+            flag[onesCount] = i;
+            onesCount++;
         }
         else
-            flag[onesCount]=0;
+            flag[onesCount] = 0;
     }
-    if(query==0){
-        for(int j=0;j<flag[0];j++){
-            printf("%c",s[j]);
+    if (query == 0)
+    {
+        for (int j = 0; j < flag[0]; j++)
+        {
+            printf("%c", s[j]);
         }
         printf("\t");
-        for(int z=0;z<flag[1];z++){
-            printf("%c",s[z]);
+        for (int z = 0; z < flag[1]; z++)
+        {
+            printf("%c", s[z]);
         }
     }
-    else if(query==1){
-        for(int z=flag[0]+1;s[z]!='\0';z++){
-            printf("%c",s[z]);
+    else if (query == 1)
+    {
+        for (int z = flag[0] + 1; s[z] != '\0'; z++)
+        {
+            printf("%c", s[z]);
         }
         printf("\t");
-        for(int k=(flag[1]+1);s[k]!='\0';k++){
-            printf("%c",s[k]);
+        for (int k = (flag[1] + 1); s[k] != '\0'; k++)
+        {
+            printf("%c", s[k]);
         }
-        
     }
     return 0;
 }
