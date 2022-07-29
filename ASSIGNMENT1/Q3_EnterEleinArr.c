3383a713b8427d426c1fed9fd0b4fd69f7daad39
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-29 21:59:28 +0530

    Enter element in any pos

---
+++ ASSIGNMENT1/Q3_EnterEleinArr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, pos, val;
+    printf("Enter the number of element in the array:");
+    scanf("%d", &n);
+    int *ptr = (int *)malloc((n + 1) * sizeof(int));
+    printf("Enter the element of array\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+    printf("Input array\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+    printf("\nEnter the position where you want to insert:");
+    scanf("%d", &pos);
+    printf("Enter the value you want to insert:");
+    scanf("%d", &val);
+    for (int i = n; i > (pos - 1); i--)
+    {
+        ptr[i] = ptr[i - 1];
+    }
+    ptr[pos - 1] = val;
+    printf("Output array\n");
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+
+    return 0;
+}
