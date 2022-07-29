8b7fe89f87333073b4e58ab9dc585a9c27646cc0
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-29 21:59:57 +0530

    Del ele from any pos

---
+++ ASSIGNMENT1/Q6_DelEle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, pos;
+    printf("Enter the number of element in the array:");
+    scanf("%d", &n);
+    int *ptr = (int *)malloc(n * sizeof(int));
+    printf("Enter the element of array\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+    printf("Input array\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+    printf("\nEnter the position where you want to delete:");
+    scanf("%d", &pos);
+    ptr[pos - 1] = 0;
+    printf("Output array\n");
+    for (int i = 0; i < n; i++)
+    {
+        if (ptr[i] != 0)
+        {
+            printf("%d ", ptr[i]);
+        }
+    }
+
+    return 0;
+}
