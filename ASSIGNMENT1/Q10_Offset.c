6f822a34690f93118f7e0c29fb9a0048e95478c3
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-29 23:55:02 +0530

    Offset

---
+++ ASSIGNMENT1/Q10_Offset.c
@@ -0,0 +1,41 @@
+// Himanshu Mohanty
+// 2105719
+// CSE36
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    char temp;
+    char *input = (char *)malloc(3 * sizeof(char));
+    int numInt[3];
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%c", &input[i]);
+        scanf("%c", &temp);
+        numInt[i] = (int)(input[i]);
+    }
+    int offset;
+    printf("Enter the offset:");
+    scanf("%d", &offset);
+    int num = 0;
+    if (offset > 0)
+    {
+        for (int i = 1; i <= offset; i++)
+        {
+            num = (numInt[2] + i);
+            printf("%c ", num);
+        }
+    }
+    if (offset < 0)
+    {
+        int val;
+        for (int i = 1; i <= (-offset); i++)
+        {
+            val = (-i) + 26;
+            num = (numInt[0] + (val));
+            printf("%c ", num);
+        }
+    }
+
+    return 0;
+}
