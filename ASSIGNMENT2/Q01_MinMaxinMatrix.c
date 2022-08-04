3f9b8f748c469c2789d1cfcade6fc9d3eefc6793
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-08-04 23:15:46 +0530

    Ques

--- ASSIGNMENT2/Q01_MinMaxinMatrix.c
+++ ASSIGNMENT2/Q01_MinMaxinMatrix.c
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
     printf("Enter the size of the matrix:");
     scanf("%d%d", &m, &n);
     int arr[m][n];
-    printf("\nEnter the elements of the array\n");
+    printf("\nEnter the elements of the matrix\n");
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
