c97589d160ef7a59b967d16dce9b9680cdf5b488
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-08-04 22:04:35 +0530

    Minimum and maximum number in a matrix

---
+++ ASSIGNMENT2/Q01_MinMaxinMatrix.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int m, n, min, max;
+    printf("Enter the size of the matrix:");
+    scanf("%d%d", &m, &n);
+    int arr[m][n];
+    printf("\nEnter the elements of the array\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("\nInput Matrix\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    max = min = arr[0][0];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i][j] > max)
+            {
+                max = arr[i][j];
+            }
+            else if (arr[i][j] < min)
+            {
+                min = arr[i][j];
+            }
+        }
+    }
+    printf("\nMax number in the matrix:%d\nMin number in the matrix:%d\n", max, min);
+
+    return 0;
+}
