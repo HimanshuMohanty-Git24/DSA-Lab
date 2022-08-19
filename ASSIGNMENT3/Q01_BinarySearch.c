842c9a88bae1d71aa6d4f96deb079f95dc65b8ce
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-08-19 12:40:58 +0530

    Binary search

--- ASSIGNMENT3/Q01_BinarySearch.c
+++ ASSIGNMENT3/Q01_BinarySearch.c
@@ -1,4 +1,28 @@
 #include <stdio.h>
+int binarysearch(int array[100], int n, int search)
+{
+    int first, last, middle;
+    first = 0;
+    last = n - 1;
+    middle = (first + last) / 2;
+
+    while (first <= last)
+    {
+        if (array[middle] < search)
+            first = middle + 1;
+        else if (array[middle] == search)
+        {
+            printf("%d found at location %d.\n", search, middle + 1);
+            break;
+        }
+        else
+            last = middle - 1;
+
+        middle = (first + last) / 2;
+    }
+    if (first > last)
+        printf("Not found! %d isn't present in the list.\n", search);
+}
 int main()
 {
     int c, first, last, middle, n, search, array[100];
@@ -25,26 +49,6 @@ int main()
             }
         }
     }
-    first = 0;
-    last = n - 1;
-    middle = (first + last) / 2;
-
-    while (first <= last)
-    {
-        if (array[middle] < search)
-            first = middle + 1;
-        else if (array[middle] == search)
-        {
-            printf("%d found at location %d.\n", search, middle + 1);
-            break;
-        }
-        else
-            last = middle - 1;
-
-        middle = (first + last) / 2;
-    }
-    if (first > last)
-        printf("Not found! %d isn't present in the list.\n", search);
-
+    binarysearch(array, n, search);
     return 0;
 }
