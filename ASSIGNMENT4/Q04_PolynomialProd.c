c2acfbe0d505dcb038884087ac68111d27c66fad
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-08-28 19:45:38 +0530

    Polynomial multipliaction

--- ASSIGNMENT4/Q04_PolynomialProd.c
+++ ASSIGNMENT4/Q04_PolynomialProd.c
@@ -7,16 +7,7 @@ int main()
     scanf("%d", &hd1);
     printf("Enter the order of second polynomial:");
     scanf("%d", &hd2);
-    int hd;
-    if (hd1 > hd2)
-    {
-        hd = hd1;
-    }
-    else
-    {
-        hd = hd2;
-    }
-    int *poly = (int *)calloc((hd + 1), sizeof(int));
+    int *poly = (int *)calloc((hd1 + 1), sizeof(int));
     printf("\nFIRST POLYNOMIAL\n");
     for (int i = 0; i <= hd1; i++)
     {
@@ -24,25 +15,33 @@ int main()
         scanf("%d", &poly[i]);
     }
     printf("\n\n");
-    int *poly1 = (int *)calloc((hd + 1), sizeof(int));
+    int *poly1 = (int *)calloc((hd2 + 1), sizeof(int));
     printf("\nSECOND POLYNOMIAL\n");
     for (int i = 0; i <= hd2; i++)
     {
         printf("Enter the coefficient of X^%d:", i);
         scanf("%d", &poly1[i]);
     }
-    int *polyprod = (int *)calloc((hd1 + hd2 - 1), sizeof(int));
+    int *polymul = (int *)calloc((hd1 + hd2 + 1), sizeof(int));
     for (int i = 0; i <= hd1; i++)
     {
-        for (int j = 0; i <= hd2; i++)
+        for (int j = 0; j <= hd2; j++)
         {
-            polyprod[i + j] = polyprod[i + j] + (poly[i] * poly1[j]);
+            polymul[i + j] = polymul[i + j] + (poly[i] * poly1[j]);
         }
     }
-    printf("\nFINAL POLYNOMIAL AFTER MULTIPLIACTION\n");
-    for (int i = hd; i >= 0; i--)
+    printf("\nFINAL POLYNOMIAL AFTER MULTIPLICATION\n");
+    for (int i = hd1 + hd2; i >= 0; i--)
     {
-        printf("Coefficient of X^%d:%d\n", i, polyprod[i]);
+        if (i != 0)
+        {
+            printf("%dX^%d+", polymul[i], i);
+        }
+        else
+        {
+            printf("%dX^%d", polymul[i], i);
+        }
     }
+
     return 0;
 }
